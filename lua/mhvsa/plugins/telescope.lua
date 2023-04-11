3a1540e514e2aab4266d0909ff2ce61157fa3b0c
Michał Sabiniarz <michal.sabiniarz@aiclearing.com>
2023-04-11 14:03:57 +0200

    Add telescope-ui-select.nvim

--- lua/mhvsa/plugins/telescope.lua
+++ lua/mhvsa/plugins/telescope.lua
@@ -1,4 +1,5 @@
 local function init_telescope()
+	-- Setup keymaps
 	local builtin = require("telescope.builtin")
 	vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 	vim.keymap.set("n", "<C-p>", builtin.git_files, {})
@@ -6,12 +7,16 @@ local function init_telescope()
 		builtin.grep_string({ search = vim.fn.input("Grep > ") })
 	end)
 	vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+
+	-- Setup plugins
+	local telescope = require("telescope")
+	telescope.load_extension("ui-select")
 end
 
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.1",
 	lazy = false,
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-ui-select.nvim" },
 	init = init_telescope,
 }
